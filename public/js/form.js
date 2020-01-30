a15ae9cc66ae53e2d3bf3fa46f97a9c0e773724e
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-01-30 19:59:42 +0900

    집에가자 ㅜㅜ

--- public/js/form.js
+++ public/js/form.js
@@ -12,9 +12,69 @@ class App {
 	init(){
 		$("#tab-menu > .tab2").eq(0)[0].querySelector("ul > li:last-child").classList.add("active");
 
-		document.querySelectorAll(".form-button-box > button").forEach(b=>{
-			b.addEventListener("click",(e)=> document.execCommand(b.dataset.property));
+		document.querySelectorAll(".form-button-box > .form_btn").forEach(b=>{
+			b.addEventListener("click",(e)=>{
+				document.execCommand(b.dataset.property);
+				document.querySelector(".editorDiv").focus();
+			});
 		});
+
+		document.querySelector(".bgclr").addEventListener("click",(e)=>{
+			document.querySelector("#bgc_input").click();
+
+		});
+
+		document.querySelector("#bgc_input").addEventListener("change",(e)=>{
+			document.execCommand("backColor",false,e.target.value);
+			document.querySelector(".editorDiv").focus();
+		});
+
+		document.querySelector(".clr").addEventListener("click",(e)=>{
+			document.querySelector("#color_input").click();
+
+		});
+
+		document.querySelector("#color_input").addEventListener("change",(e)=>{
+			document.execCommand("foreColor",false,e.target.value);
+			document.querySelector(".editorDiv").focus();
+		});
+
+		document.querySelector("#form-image-add").addEventListener("click",(e)=>{
+			let input = document.createElement("input");
+			input.setAttribute("type", "file");
+			input.setAttribute("multiple","");
+			input.addEventListener("change",(e)=>{
+				let fileList = Array.from(e.target.files);
+				fileList.forEach(f=>{
+					let img = new Image();
+					let type = f.type.split("/")[0];
+					if(type=="image"){ // 타입이 이미지임
+						let reader = new FileReader();
+						reader.readAsDataURL(f);
+						reader.addEventListener("load",(e)=>{ // base64 완료
+							let div = this.getImgPreviewTemp(f.name,reader.result);
+							document.querySelector(".form-image-box").prepend(div);
+						},false);
+					} else {
+						log(f);
+					}
+				});
+			});
+			input.click();
+		});
+	}
+
+	getImgPreviewTemp(name,src){
+		let div = document.createElement("div");
+		div.classList.add("form-image-item-box");
+		div.innerHTML = `
+		<img src="${src}" alt="${name}" title="${name}">
+		<div class="form-image-item-close">&times;</div>
+		`;
+		div.querySelector(".form-image-item-close").addEventListener("click",(e)=>{
+			$(div).remove();
+		});
+		return div;
 	}
 
 }
--- views/form.php
+++ views/form.php
@@ -1,158 +1,47 @@
 <script src="/js/form.js"></script>
 <section id="main">
+	<?php  // echo $_SERVER['REMOTE_ADDR']; ?>
+	<input type="color" id="bgc_input">
+	<input type="color" id="color_input">
+	<input type="file" id="form_file" multiple>
 	<div class="form-form-box">
 		<div class="form-form-container">
 			<div class="form-form-title">글쓰기</div>
 			<div class="form-button-box">
-				<button class="bld" data-property="bold">B</button>
-				<button class="it" data-property="Italic">T</button>
-				<button class="udl" data-property="Underline">U</button>
-				<button class="strk" data-property="StrikeThrough">S</button>
-				<button class="jstfl" data-property="justifyleft">
+				<button class="bld form_btn" data-property="bold">B</button>
+				<button class="it form_btn" data-property="Italic">T</button>
+				<button class="udl form_btn" data-property="Underline">U</button>
+				<button class="strk form_btn" data-property="StrikeThrough">S</button>
+				<button class="jstfl form_btn" data-property="justifyleft">
 					<i class="fas fa-align-left"></i>
 				</button>
-				<button class="jstfc" data-property="justifycenter">
+				<button class="jstfc form_btn" data-property="justifycenter">
 					<i class="fas fa-align-center"></i>
 				</button>
-				<button class="jstfr" data-property="justifyright">
+				<button class="jstfr form_btn" data-property="justifyright">
 					<i class="fas fa-align-right"></i>
+				</button>
+				<button class="bgclr form_btn_color" data-property="backColor">
+					<i class="fas fa-fill-drip"></i>
+				</button>
+				<button class="clr form_btn_color" data-property="foreColor">
+					<i class="fas fa-tint"></i>
 				</button>
 			</div>
 			<div class="editorDiv" contenteditable="true" placeholder="새로운 소식을 남겨보세요." spellcheck="false"></div>
 			<div class="form-image-box">
-
-
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
-				<div class="form-image-item-box">
-					<img src="/imgs/vkeod1.jpg" alt="">
-					<div class="form-image-item-close">&times;</div>
-				</div>
+				
 				<div class="form-image-item-box">
 					<img src="/imgs/vkeod1.jpg" alt="">
 					<div class="form-image-item-close">&times;</div>
 				</div>
 
-				<button id="form-image-add">
+				<button type="button" id="form-image-add">
 					<i class="fas fa-plus"></i>
 				</button>
 			</div>
 			<div class="form-bottom">
-				<button class="form-submit">게시</button>
+				<button type="submit" class="form-submit">게시</button>
 			</div>
 		</div>
 	</div>
